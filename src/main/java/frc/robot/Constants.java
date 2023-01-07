f3c598a105829f641c19b6d9beaf03b9e230b02d
Dylan Isaac <dylanisaac2005@gmail.com>
2023-01-07 07:29:15 -0500

    Corrected offsets and ports

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,11 +2,11 @@ package frc.robot;
 
 public class Constants {
     public static class DriveConstants {
-        public static int[] swerveMotorDrivePorts = {1, 4, 7, 10}; //TODO: update port numbers
-        public static int[] swerveMotorTurnPorts = {2, 5, 8, 11}; //TODO: update port numbers
+        public static int[] swerveMotorDrivePorts = {2, 5, 8, 11}; //TODO: update port numbers
+        public static int[] swerveMotorTurnPorts = {1, 4, 7, 10}; //TODO: update port numbers
         public static int[] swerveEncoderPorts = {3, 6, 9, 12}; //TODO: update port numbers
 
-        public static float[] turnAngleOffsets = {0, 0, 0, 0}; //degrees
+        public static double[] turnAngleOffsets = {-26.0695796785, 40.9664823519,-82.3340351603, 17.0168465154}; //degrees
         public static boolean[] turnEncoderInversed = {false, false, false, false};
 
         public static double turnPID_P = 0.5;
